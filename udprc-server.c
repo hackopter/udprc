fde9c0a433189f97825d64d941e662319174b106
Vasilis Tsiligiannis <b_tsiligiannis@silverton.gr>
2013-06-12 23:46:08 +0300

    Cleanup and fix indentation

--- udprc-server.c
+++ udprc-server.c
@@ -15,9 +15,7 @@ int main (int argc, char **argv) {
     int sock;
     struct sockaddr_in saddr, caddr;
     socklen_t addrlen = sizeof(struct sockaddr_in);
-   // uint8_t buf[BUF_SIZ];
-   //double buf[BUF_SIZ];
-   int16_t buf[BUF_SIZ];
+    int16_t buf[BUF_SIZ];
 
     sock = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -50,8 +48,7 @@ int main (int argc, char **argv) {
 	    exit (-1);
 	}
 
-//printf("%.f %.f %.f\n", buf[0], buf[1], buf[2]);
-printf("%d %d %d %d\n", buf[0], buf[1], buf[2], buf[3]);
+	printf("%d %d %d %d\n", buf[0], buf[1], buf[2], buf[3]);
 	    
     }
 
